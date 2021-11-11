7817664c13ebcd40a439f0cd789bb75c8c08f49b
HoanNg96 <nhhoan96@gmail.com>
2021-11-11 23:26:40 +0700

    checkout frontend

---
+++ README.md
@@ -0,0 +1,17 @@
+# CustomCheckout
+
+1. Add 1 step in when checkout for customer to type delivery date and note
+- delivery date is required
+- admin can see and modify delivery date and note in backend (order detail)
+
+# Construction step
+- create module (di Magento_Checkout)
+- add delivery date & note column in quote & sales_order table in database
+
+* FE:
+- add new checkout step:
+    + create js file in <your_module_dir>/view/frontend/web/js/view
+    + create html file in <your_module_dir>/view/frontend/web/template
+    + add new step to checkout page layout
+- add controller to save form data to quote table
+- add observer to save form data to sales_order table
---
+++ view/frontend/web/js/view/delivery-step.js
@@ -0,0 +1,108 @@
+define(
+    [
+        'jquery',
+        'ko',
+        'uiComponent',
+        'underscore',
+        'Magento_Checkout/js/model/step-navigator',
+        'Magento_Customer/js/model/customer',
+        'Magento_Checkout/js/model/quote',
+        'mage/url',
+        'mage/storage'
+    ],
+    function (
+        $,
+        ko,
+        Component,
+        _,
+        stepNavigator,
+        customer,
+        quote,
+        urlBuilder,
+        storage
+    ) {
+        'use strict';
+        /**
+        * delivery - is the name of the component's .html template
+        * AHT_CustomCheckout - is the name of your module directory.
+        */
+        return Component.extend({
+            defaults: {
+                template: 'AHT_CustomCheckout/delivery'
+            },
+
+            //add here your logic to display step,
+            isVisible: ko.observable(true),
+            /* isLogedIn: customer.isLoggedIn(), */
+            //step code will be used as step content id in the component template
+            stepCode: 'delivery', /* correspond html file name in sibling template folder */
+            //step title value
+            stepTitle: 'Delivery step',
+
+            /**
+            *
+            * @returns {*}
+            */
+            initialize: function () {
+                this._super();
+                // register your step
+                stepNavigator.registerStep(
+                    // step code will be used as step content id in the component template
+                    this.stepCode,
+                    //step alias
+                    null,
+                    // step title value
+                    this.stepTitle,
+                    //observable property with logic when display step or hide step
+                    this.isVisible,
+
+                    _.bind(this.navigate, this),
+
+                    /**
+                    * sort order value
+                    * 'sort order value' < 10: step displays before shipping step;
+                    * 10 < 'sort order value' < 20 : step displays between shipping and payment step
+                    * 'sort order value' > 20 : step displays after payment step
+                    */
+                    15
+                );
+
+                return this;
+            },
+
+            /**
+            * The navigate() method is responsible for navigation between checkout step
+            * during checkout. You can add custom logic, for example some conditions
+            * for switching to your custom step
+            */
+            navigate: function () {
+                this.isVisible(true); // show custom step content when user navigates via url anchor or back button
+            },
+
+            /**
+            * @returns void
+            */
+            navigateToNextStep: function () {
+                // get date value
+                var date = $("input[name=date]").val();
+                // get comment value
+                var comment = $("#delivery-comment").val();
+                // get current quote id
+                var quoteId = quote.getQuoteId();
+                // controller url to send data
+                var url = urlBuilder.build('delivery/index/savequote');
+                storage.post(
+                    url,
+                    JSON.stringify({ quoteId: quoteId, date: date, comment: comment }), // send data as JSON
+                    false
+                ).done(
+                    function () {
+                        stepNavigator.next();
+                    }
+                ).fail(
+                );
+
+            }
+        });
+    }
+);
